785291c92c83725adddc71a75b46abb6e3a6f013
Rinta Setyo Nugroho <rinta.setyonugroho@gmail.com>
2025-01-28 11:14:08 +0700

    update form login styling

--- src/app/login/index.tsx
+++ src/app/login/index.tsx
@@ -16,15 +16,6 @@ const LoginPage: React.FC = () => {
   return (      
     <div className="flex items-center justify-center min-h-screen bg-transparent relative overflow-hidden">      
       <Login baseUrl={BASE_URL} onRegisterClick={handleRegisterRedirect} router={router} />      
-          
-      {/* Elemen Bola */}    
-      <div className="bola" style={{ top: '20%', left: '10%' }}></div>    
-      <div className="bola" style={{ top: '50%', left: '70%' }}></div>    
-      <div className="bola" style={{ top: '80%', left: '30%' }}></div>    
-    
-      {/* Elemen Persegi */}    
-      <div className="square" style={{ top: '30%', left: '80%' }}></div>    
-      <div className="square" style={{ top: '60%', left: '20%' }}></div>    
     </div>      
   );      
 };      
